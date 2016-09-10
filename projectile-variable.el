c76dc27c16d25a6280fa6ace7fc9ecb1922f16cc
USAMI Kenta <tadsan@zonu.me>
2016-09-11 01:52:08 +0900

    Update README

--- projectile-variable.el
+++ projectile-variable.el
@@ -26,11 +26,14 @@
 
 ;;; Commentary:
 
-;; Store project local variables (property).
+;; Store project local variables (property) using Projectile and Symbol Plists.
+;;
+;; - https://github.com/bbatsov/projectile
+;; - https://www.gnu.org/software/emacs/manual/html_node/elisp/Symbol-Plists.html
 
 ;; (projectile-variable-put 'foo-value 2) ;; Store property
 ;; (projectile-variable-get 'foo-value)   ;;=> 2
-
+;;
 ;; (projectile-variable-plist)        ;; Return all project local property list
 ;; (projectile-variable-plist "foo-") ;; Return project local property list filterd by prefix "foo-"
 ;; (projectile-variable-alist)        ;; Return all project local properties as association list (alist)
